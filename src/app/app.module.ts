710bec157f950bf5f1515c2440377c19fbbb97eb
mrrgoodcat <mrrgoodcat@gmail.com>
2023-07-12 03:06:41 +0300

    Login and home pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent,
